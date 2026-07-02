707dbaffeef0a371fce705dae9b680add713030d
Ștefan <stefanqq@proton.me>
2026-07-02 23:12:44 +0000

    fix: dynamically compress prebuilt kernel and drop crypto to fix size overflow

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,15 +34,18 @@ TARGET_NO_BOOTLOADER := true
 # Display
 TARGET_SCREEN_DENSITY := 280
 
+# Dynamic Kernel Compression Hack (Saves massive space if the prebuilt kernel is uncompressed)
+$(shell if od -An -N2 -t x1 $(DEVICE_PATH)/prebuilt/kernel | grep -q '1f 8b'; then cp $(DEVICE_PATH)/prebuilt/kernel $(DEVICE_PATH)/prebuilt/kernel.gz; else gzip -9 -c $(DEVICE_PATH)/prebuilt/kernel > $(DEVICE_PATH)/prebuilt/kernel.gz; fi)
+
 # Kernel
 BOARD_KERNEL_BASE := 0x10000000
-BOARD_KERNEL_CMDLINE := androidboot.selinux=permissive androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := androidboot.selinux=permissive
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_RAMDISK_OFFSET := 0x01000000
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
 BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
-BOARD_KERNEL_IMAGE_NAME := Image
+BOARD_KERNEL_IMAGE_NAME := Image.gz
 BOARD_KERNEL_SEPARATED_DT := true
 TARGET_KERNEL_CONFIG := j7duolte_defconfig
 TARGET_KERNEL_SOURCE := kernel/samsung/j7duolte
@@ -50,7 +53,7 @@ TARGET_KERNEL_SOURCE := kernel/samsung/j7duolte
 # Kernel - prebuilt
 TARGET_FORCE_PREBUILT_KERNEL := true
 ifeq ($(TARGET_FORCE_PREBUILT_KERNEL),true)
-TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel.gz
 TARGET_PREBUILT_DT := $(DEVICE_PATH)/prebuilt/dt.img
 BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DT)
 BOARD_KERNEL_SEPARATED_DT := 
@@ -78,9 +81,6 @@ PLATFORM_SECURITY_PATCH := 2099-12-31
 VENDOR_SECURITY_PATCH := 2099-12-31
 PLATFORM_VERSION := 16.1.0
 
-# Force Global LZMA Compression
-BOARD_RAMDISK_COMPRESSION := lzma
-
 # TWRP Trim Configs
 TW_THEME := portrait_hdpi
 TW_EXTRA_LANGUAGES := false
@@ -98,6 +98,11 @@ TW_EXCLUDE_TWRPAPP := true
 TW_EXCLUDE_LOGCAT := true
 TW_NO_EXFAT := true
 TW_NO_EXFAT_FUSE := true
+TW_INCLUDE_CRYPTO := false
+TW_INCLUDE_CRYPTO_FBE := false
+TW_EXCLUDE_ENCRYPTED_BACKUPS := true
+TW_EXCLUDE_RESETPROP := true
+TW_EXCLUDE_ANYKERNEL := true
 
 # TWRP Configuration
 TW_SCREEN_BLANK_ON_BOOT := true
