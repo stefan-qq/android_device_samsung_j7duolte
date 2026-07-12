7a7381a1ff2542da49ecdf90752838b9a854946e
Mardari Ștefan <stefanqq@proton.me>
2026-07-12 23:02:29 +0300

    recovery: use stock FunctionFS ADB flow for exynos7884

--- device.mk
+++ device.mk
@@ -1,8 +1,17 @@
-#
 # Copyright (C) 2026 The Android Open Source Project
-# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
-#
 # SPDX-License-Identifier: Apache-2.0
-#
 
-LOCAL_PATH := device/samsung/j7duolte
+# Files under recovery/root are copied by the recovery build itself. Do not add
+# duplicate PRODUCT_COPY_FILES destinations for those files.
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware=exynos7884 \
+    ro.product.board=exynos7884 \
+    ro.board.platform=universal7884 \
+    ro.product.device=j7duolte \
+    ro.product.model=SM-J720F \
+    ro.product.manufacturer=samsung \
+    ro.secure=0 \
+    ro.adb.secure=0 \
+    ro.debuggable=1 \
+    persist.sys.usb.config=adb
