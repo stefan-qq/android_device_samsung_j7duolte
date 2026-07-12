b1bfe108004880afac6427da966df8c9e5c59950
Ștefan <stefanqq@proton.me>
2026-07-12 21:11:03 +0000

    build: inherit Omni configuration for twrp-9

--- omni_j7duolte.mk
+++ omni_j7duolte.mk
@@ -3,7 +3,7 @@
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
-$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 $(call inherit-product, device/samsung/j7duolte/device.mk)
 
 PRODUCT_DEVICE := j7duolte
